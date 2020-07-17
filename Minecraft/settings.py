33f4756a64ad6d94703d83ddf29a498874272fc5
ULUNTA <ULUNTA@gmail.com>
2020-07-18 05:31:24 +0900

    change DB

--- Minecraft/settings.py
+++ Minecraft/settings.py
@@ -79,14 +79,14 @@ WSGI_APPLICATION = 'Minecraft.wsgi.application'
 
 DATABASES = {
     'default': {
-        #'ENGINE': 'django.db.backends.sqlite3',
-        #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'Minecraft_DB',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres',
-        'HOST': '127.0.0.1',
-        'POST': '5432'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        #'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        #'NAME': 'Minecraft_DB',
+        #'USER': 'postgres',
+        #'PASSWORD': 'postgres',
+        #'HOST': '127.0.0.1',
+        #'POST': '5432'
     }
 }
 
